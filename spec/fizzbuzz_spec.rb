88349dd36a72f78366023451cfd58387228d29ee
Hanif Fakira <fakira.work@gmail.com>
2016-02-22 14:22:46 +0000

    finished fizzbuzz

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../lib/fizzbuzz'
+
+describe '#fizzbuzz' do
+  it 'returns "fizz" when passed 3' do
+    expect(fizzbuzz(3)).to eq 'fizz'
+  end
+
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+
+  it 'returns "fizzbuzz" when passed 3 and 5' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+
+  it 'returns number when passed a number not divisible by 3 or 5' do
+    expect(fizzbuzz(1)).to eq 1
+  end
+end
